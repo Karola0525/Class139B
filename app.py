82c734c100b1f772b2b2ac4d637d7f19b290c3d8
Karola0525 <114681978+Karola0525@users.noreply.github.com>
2024-01-25 16:00:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -48,54 +48,7 @@ def predict_emotion():
         # Enviar respuesta.         
         return jsonify(response)
 
-#funcion que lee el archivo csv con el hsitorial de predicciones y lo devuelve en un arreglo
-def show_entry():
-    day_entry_list = pd.read_csv("/content/Class139B/static/assets/data_files/data_entry.csv")
-    day_entry_list = day_entry_list.iloc[::-1]
 
-    date1 = (day_entry_list['date'].values[0])
-    date2 = (day_entry_list['date'].values[1])
-    date3 = (day_entry_list['date'].values[2])
-
-    entry1 = (day_entry_list['text'].values[0])
-    entry2 = (day_entry_list['text'].values[1])
-    entry3 = (day_entry_list['text'].values[2])
-
-    emotion1 = (day_entry_list['emotion'].values[0])
-    emotion2 = (day_entry_list['emotion'].values[1])
-    emotion3 = (day_entry_list['emotion'].values[2])
-
-    emotion_url_1 = ""
-    emotion_url_2 = ""
-    emotion_url_3 = ""
-
-    for key, value in emo_code_url.items():
-        if key==emotion1:
-            emotion_url_1 = value[1]
-        if key==emotion2:
-            emotion_url_2 = value[1]
-        if key==emotion3:
-            emotion_url_3 = value[1]
-    return [
-        {
-            "date": date1,
-            "entry": entry1,
-            "emotion": emotion1,
-            "emotion_url": emotion_url_1
-        },
-        {
-            "date": date2,
-            "entry": entry2,
-            "emotion": emotion2,
-            "emotion_url": emotion_url_2
-        },
-        {
-            "date": date3,
-            "entry": entry3,
-            "emotion": emotion3,
-            "emotion_url": emotion_url_3
-        }
-    ]
 @app.route("/save-entry", methods=["POST"])
 def save_entry():
 
