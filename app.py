dd5e7044307bb7a682e7936b1aac8098a106ba26
Karola0525 <114681978+Karola0525@users.noreply.github.com>
2024-01-25 15:57:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, url_for, request, jsonify
 from model_prediction import * 
 from predict_response import *
+
+#libreria para exponer la api a una url publica
+from flask_ngrok import run_with_ngrok
+
  
 app = Flask(__name__)
 
 predicted_emotion=""
 predicted_emotion_img_url=""
+
+# correr la api en url publica
+run_with_ngrok(app)
+
 
 @app.route('/')
 def index():
@@ -40,7 +48,54 @@ def predict_emotion():
         # Enviar respuesta.         
         return jsonify(response)
 
+#funcion que lee el archivo csv con el hsitorial de predicciones y lo devuelve en un arreglo
+def show_entry():
+    day_entry_list = pd.read_csv("/content/Class139B/static/assets/data_files/data_entry.csv")
+    day_entry_list = day_entry_list.iloc[::-1]
 
+    date1 = (day_entry_list['date'].values[0])
+    date2 = (day_entry_list['date'].values[1])
+    date3 = (day_entry_list['date'].values[2])
+
+    entry1 = (day_entry_list['text'].values[0])
+    entry2 = (day_entry_list['text'].values[1])
+    entry3 = (day_entry_list['text'].values[2])
+
+    emotion1 = (day_entry_list['emotion'].values[0])
+    emotion2 = (day_entry_list['emotion'].values[1])
+    emotion3 = (day_entry_list['emotion'].values[2])
+
+    emotion_url_1 = ""
+    emotion_url_2 = ""
+    emotion_url_3 = ""
+
+    for key, value in emo_code_url.items():
+        if key==emotion1:
+            emotion_url_1 = value[1]
+        if key==emotion2:
+            emotion_url_2 = value[1]
+        if key==emotion3:
+            emotion_url_3 = value[1]
+    return [
+        {
+            "date": date1,
+            "entry": entry1,
+            "emotion": emotion1,
+            "emotion_url": emotion_url_1
+        },
+        {
+            "date": date2,
+            "entry": entry2,
+            "emotion": emotion2,
+            "emotion_url": emotion_url_2
+        },
+        {
+            "date": date3,
+            "entry": entry3,
+            "emotion": emotion3,
+            "emotion_url": emotion_url_3
+        }
+    ]
 @app.route("/save-entry", methods=["POST"])
 def save_entry():
 
@@ -54,7 +109,7 @@ def save_entry():
     # Entrada CSV. 
     entry = f'"{date}","{save_text}","{emotion}"\n'  
 
-    with open("./static/assets/data_files/data_entry.csv", "a") as f:
+    with open("/content/Class139B/static/assets/data_files/data_entry.csv", "a") as f:
         f.write(entry)
     return jsonify("Success")
 
@@ -73,5 +128,4 @@ def bot():
 
     return jsonify(response)     
      
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run()
